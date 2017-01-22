0fdce2d41e866ab00bf5e84aadf1a93186bec4d2
Scott La Fetra <scottlafetra@protonmail.com>
2017-01-21 17:59:38 -0800

    Minor Tweaks

--- Assets/Scripts/EventOnTriggerEnter.cs
+++ Assets/Scripts/EventOnTriggerEnter.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EventOnTriggerEnter : MonoBehaviour {
-    public string exceptionTag = "";
+    public List<string> exceptionTags = new List<string>();
 
     public delegate void NoodleTriggeredHandler();
     public event NoodleTriggeredHandler triggered;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if( other == null || other.tag != exceptionTag ) // if manually triggered or not an exception
+        if( other == null || !exceptionTags.Contains( other.tag ) ) // if manually triggered or not an exception
         {
             triggered();
         }
